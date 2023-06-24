5f13bc6c889d53c8106aa5f50f1ac9a8b7e6c8c2
Tiri <shaymusts@gmail.com>
2023-06-24 22:24:45 +0200

    check for active sessions

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,38 @@
 import { AppShell } from '@mantine/core'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Dashboard from '../../components/Dashboard'
 import Header from '../../components/Header'
 import Navbar from '../../components/Navbar'
 import { SearchResults } from '../search'
 import { useStore } from '../../store'
 import { useStyles } from './appshell.styles'
+import { User } from '@supabase/supabase-js'
+import { supabase } from '../../api/supabase'
 
 export const HomePage: FC = () => {
+  const setToken = useStore(state => state.setToken)
+  const [user, setUser] = useState<User | null>(null)
   const query = useStore(state => state.query)
   const { classes } = useStyles()
+
+  useEffect(() => {
+    async function getUserSession() {
+      const { data: { session } } = await supabase.auth.getSession()
+      const { data: { user } } = await supabase.auth.getUser()
+      if(session) {
+        setToken(session.access_token)
+        setUser(user)
+      } else {
+        setToken(null)
+      }
+    }
+    getUserSession()
+
+  }, [setToken])
+
   return (
     <AppShell
-      navbar={<Navbar />}
+      navbar={<Navbar user={user}/>}
       header={<Header />}
       navbarOffsetBreakpoint="md"
       className={classes.root}
